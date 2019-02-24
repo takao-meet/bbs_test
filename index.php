2864413b62c3960349202cbb6b93460679fb4c76
takao <takao.netnet@gmail.com>
2019-02-24 21:09:56 +0900

    ファースト

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>ひとこと掲示板</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+<div id="wrap">
+  <div id="head">
+    <h1>ひとこと掲示板</h1>
+  </div>
+  <div id="content">
+    <form action="" method="post">
+    <textarea name="message" cols="50" rows="5"></textarea>
+    <input type="hidden" name="reply_post_id" value="" />
+      <div>
+        <p>
+          <input type="submit" value="投稿する" />
+        </p>
+      </div>
+    </form>
+  </div>
+</div>
+</body>
+</html>
